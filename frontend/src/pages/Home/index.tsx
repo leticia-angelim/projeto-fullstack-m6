0f646026b284ffd05d9a91aef40483ae3635626a
Leticia Angelim <leticia.angelim@hotmail.com>
2023-02-23 11:20:30 -0300

    feat: add home page

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,7 +1,39 @@
 import React from "react";
+import Button from "../../components/Button";
+import { ProductCard } from "../../components/ProductCard";
+import Slider from "../../components/Slider";
+import { Buttons, Container } from "./styles";
 
 const Home = () => {
-  return <h1>Home</h1>;
+  return (
+    <Container>
+      <div className="box-container">
+        <div className="box">
+          <h2>Velocidade e experiência em um lugar feito para você</h2>
+          <p>Um ambiente feito para você explorar o seu melhor</p>
+        </div>
+        <Buttons>
+          <Button
+            backgroundColor="inherit"
+            backgroundColorHover="#FDFDFD"
+            border="#FDFDFD"
+            fontColorHover=" #0B0D0D"
+            children="Carros"
+          />
+          <Button
+            backgroundColor="inherit"
+            backgroundColorHover="#FDFDFD"
+            border="#FDFDFD"
+            fontColorHover=" #0B0D0D"
+            children="Motos"
+          />
+        </Buttons>
+      </div>
+      <Slider title="Leilão" children={<ProductCard />} />
+      <Slider title="Carros" children={<ProductCard />} />
+      <Slider title="Motos" children={<ProductCard />} />
+    </Container>
+  );
 };
 
 export default Home;
---
+++ frontend/src/pages/Home/styles.ts
@@ -0,0 +1,65 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  .box-container {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    gap: 3.5rem;
+
+    height: 581px;
+    background: var(--color-brand2);
+
+    .box {
+      display: flex;
+      flex-direction: column;
+      gap: 2rem;
+
+      max-width: 748px;
+      width: 100%;
+
+      h2 {
+        font-family: "Lexend";
+        font-style: normal;
+        font-size: 2.5rem;
+        text-align: center;
+        color: var(--color-grey10);
+      }
+
+      p {
+        font-family: "Inter";
+        font-style: normal;
+        font-weight: 400;
+        font-size: 16px;
+        text-align: center;
+        color: var(--color-grey9);
+      }
+    }
+  }
+`;
+
+export const Buttons = styled.div`
+  display: flex;
+  gap: 21px;
+
+  width: 381.04px;
+  height: 48px;
+
+  button {
+    width: 100%;
+  }
+
+  @media (max-width: 413px) {
+    flex-direction: column;
+    width: 351px;
+    height: 117px;
+
+    button {
+      height: 48px;
+    }
+  }
+`;
