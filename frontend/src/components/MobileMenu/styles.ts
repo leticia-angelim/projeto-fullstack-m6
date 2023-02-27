56d1f76f7df9f65a60a9eaf3d704692638ee0f53
Thiago AS <thiago-pin@gotmail.com>
2023-02-27 12:01:27 -0300

    feat: rename component

--- frontend/src/components/MobileMenu/styles.ts
+++ frontend/src/components/MobileMenu/styles.ts
@@ -37,38 +37,40 @@ export const MenuDropdownSt = styled.div<MenuProps>`
     z-index: 1;
     box-shadow: 0px 4px 40px -10px rgba(0, 0, 0, 0.25);
 
-    > ul {
+    .menu-list {
       width: 100%;
       background-color: var(--color-whiteFixed);
-    }
-    li {
-      padding: 1.5rem 1rem;
-    }
-    li a {
-      font-size: 1rem;
-      text-decoration: none;
-      font-family: "Inter", sans-serif;
-      font-weight: 600;
-      color: var(--color-grey2);
-    }
+      padding-left: 0rem;
 
-    li:nth-child(4) {
-      padding: 2rem 1rem 1rem 1rem;
-      border-top: 1px solid var(--color-grey4);
-    }
+      li {
+        padding: 1.5rem 1rem;
+      }
+      li a {
+        font-size: 1rem;
+        text-decoration: none;
+        font-family: "Inter", sans-serif;
+        font-weight: 600;
+        color: var(--color-grey2);
+      }
 
-    li button {
-      width: 100%;
-      padding: 0.75rem 1.75rem;
-      font-family: "Inter", sans-serif;
-      font-size: 1rem;
-      font-weight: 600;
-      background: var(--color-whiteFixed);
-      border: 1px solid var(--color-grey4);
-      border-radius: 4px;
-      cursor: pointer;
-      :hover {
-        background-color: var(--color-grey8);
+      li:nth-child(4) {
+        padding: 2rem 1rem 1rem 1rem;
+        border-top: 1px solid var(--color-grey4);
+      }
+
+      li button {
+        width: 100%;
+        padding: 0.75rem 1.75rem;
+        font-family: "Inter", sans-serif;
+        font-size: 1rem;
+        font-weight: 600;
+        background: var(--color-whiteFixed);
+        border: 1px solid var(--color-grey4);
+        border-radius: 4px;
+        cursor: pointer;
+        :hover {
+          background-color: var(--color-grey8);
+        }
       }
     }
   }
