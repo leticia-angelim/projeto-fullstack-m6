7b04459eeea058ef577e0bf46110bb77ad5d4619
Thiago AS <thiago-pin@gotmail.com>
2023-02-27 11:25:36 -0300

    style: nav bar padding

--- frontend/src/components/MenuDropdown/styles.ts
+++ frontend/src/components/MenuDropdown/styles.ts
@@ -36,7 +36,8 @@ export const MenuDropdownSt = styled.div<MenuProps>`
     right: 0;
     z-index: 1;
     box-shadow: 0px 4px 40px -10px rgba(0, 0, 0, 0.25);
-    ul {
+
+    > ul {
       width: 100%;
       background-color: var(--color-whiteFixed);
     }
