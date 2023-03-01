ceb067040168ea7df2ec952c56dbf16094d8da65
Thiago AS <thiago-pin@gotmail.com>
2023-03-01 13:48:29 -0300

    fix: edit user schema

--- frontend/src/schemas/editUser.tsx
+++ frontend/src/schemas/editUser.tsx
@@ -1,19 +1,10 @@
 import * as yup from "yup";
 
 export const editUserSchema = yup.object({
-  // name: yup.string().required("Campo obrigratório"),
-  // email: yup.string().required("Campo obrigratório").email("Email inválido"),
-  // cpf: yup
-  //   .string()
-  //   .required("Campo obrigratório")
-  //   .matches(/^\d{3}\.\d{3}\.\d{3}\-\d{2}$/, "CPF inválido"),
-  // phone: yup.string().required("Campo obrigratório"),
   birth_date: yup
     .string()
-    .required("Campo obrigratório")
     .matches(
       /^(0?[1-9]|[12][0-9]|3[01])[\/\-](0?[1-9]|1[012])[\/\-]\d{4}$/,
       "Data deve ser no formato DD/MM/YYYY"
     ),
-  // description: yup.string().required("Campo obrigratório"),
 });
