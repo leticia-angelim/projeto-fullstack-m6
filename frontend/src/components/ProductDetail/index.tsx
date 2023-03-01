a62a52451d460afd3b4f63c12b4e565df4ad9d39
Thiago AS <thiago-pin@gotmail.com>
2023-03-01 17:55:23 -0300

    fix: comments removed

--- frontend/src/components/ProductDetail/index.tsx
+++ frontend/src/components/ProductDetail/index.tsx
@@ -17,14 +17,10 @@ import PhotoModal from "../PhotoModal";
 import Button from "../../components/Button";
 import stringToColor from "../../util/stringToColor";
 import nameAbbreviate from "../../util/nameAbbreviate";
-
-import { CommentsList } from "../CommentsList";
-
 import { UserContext } from "../../contexts/UserContext";
 import { AnnouncementContext } from "../../contexts/AnnouncementContext";
 import { useNavigate } from "react-router-dom";
 import { CommentsList } from "../CommentsList";
-
 
 const ProductDetail = () => {
   const { selectedAnnouncement, setPhotoModal, setSelectedPhoto } =
