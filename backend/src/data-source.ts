b8b92bfb1e4a883c9de3b7b7d1330f72b446a814
Leticia Angelim <103081652+leticia-angelim@users.noreply.github.com>
2023-03-06 19:22:18 -0300

    refactor: data-source

--- backend/src/data-source.ts
+++ backend/src/data-source.ts
@@ -7,12 +7,29 @@ import { Address } from "./entities/address.entity";
 import { Comment } from "./entities/comments.entity";
 import { createEntities1677766971545 } from "./migrations/1677766971545-createEntities";
 
-const AppDataSource = new DataSource({
-  type: "postgres",
-  url: process.env.DATABASE_URL,
-  entities: [User, Announcement, Photo, Address, Comment],
-  migrations: [createEntities1677766971545],
-});
+const nodeEnv: string | undefined = process.env.NODE_ENV;
+
+const AppDataSource = new DataSource(
+  nodeEnv === "production"
+    ? {
+        type: "postgres",
+        url: process.env.DATABASE_URL,
+        entities: [User, Announcement, Photo, Address, Comment],
+        migrations: [createEntities1677766971545],
+      }
+    : {
+        type: "postgres",
+        host: "localhost",
+        port: 5432,
+        username: process.env.POSTGRES_USER,
+        password: process.env.POSTGRES_PASSWORD,
+        database: process.env.POSTGRES_DB,
+        synchronize: false,
+        logging: true,
+        entities: [User, Announcement, Photo, Address, Comment],
+        migrations: [createEntities1677766971545],
+      }
+);
 
 AppDataSource.initialize()
   .then(() => {
