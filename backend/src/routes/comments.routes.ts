56b37bbb4af069c0a34b9d76220ee9d4644984a3
Thiago AS <thiago-pin@gotmail.com>
2023-03-06 10:09:55 -0300

    feat: update comment and middleware done

--- backend/src/routes/comments.routes.ts
+++ backend/src/routes/comments.routes.ts
@@ -2,13 +2,26 @@ import { Router } from "express";
 import createCommentController from "../controllers/comments/createComment.controller";
 import deleteCommentController from "../controllers/comments/deleteComment.controller";
 import listCommentController from "../controllers/comments/listComment.controller";
+import updateCommentController from "../controllers/comments/updateComment.controller";
 
 import ensureAuthMiddleware from "../middlewares/ensureAuth.middleware";
+import verifyCommentOwner from "../middlewares/verifyCommentOwner.middleware";
 
 const commentsRoutes = Router();
 
 commentsRoutes.post("/:id", ensureAuthMiddleware, createCommentController);
 commentsRoutes.get("/:id", listCommentController);
-commentsRoutes.delete("/:id", ensureAuthMiddleware, deleteCommentController);
+commentsRoutes.patch(
+  "/:id",
+  ensureAuthMiddleware,
+  verifyCommentOwner,
+  updateCommentController
+);
+commentsRoutes.delete(
+  "/:id",
+  ensureAuthMiddleware,
+  verifyCommentOwner,
+  deleteCommentController
+);
 
 export default commentsRoutes;
